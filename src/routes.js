0a6749d7726d9b395aedeab7b16f3b78e71e967e
Guilherme Rocha <guilherme.dev12@gmail.com>
2023-12-12 20:00:26 -0300

    test message commit

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,6 @@ import { createRouter, createWebHistory } from 'vue-router'
 import singlePosts from './components/singlePost.vue'
 import postsList from './components/postsList.vue';
 
-// const extrairParametroId = route => ({
-//     id: +route.params.id
-// })
-
 const routes = [
     {path: '/:id', 
         component:  singlePosts,
